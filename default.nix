cd9aba49d8801ed84f9960a6966f0d0aff9c363d
Chris S <chris@soundshare.co.uk>
2020-05-10 13:20:20 +0100

    nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ sources ? import ./nix/sources.nix
+, compiler ? "ghc865" } :
+let
+  niv = import sources.nixpkgs {
+    overlays = [
+      (_ : _ : { niv = import sources.niv {}; })
+    ] ;
+    config = {};
+  };
+  pkgs = niv.pkgs;
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+    };
+  };
+in
+myHaskellPackages.callCabal2nix "Haskellnet" (./.) {}
